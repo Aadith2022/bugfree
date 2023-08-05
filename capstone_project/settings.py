796acd87455be9b9e50a2d76d6bbedfa65e3e5d4
[Aadith Samala] <[aadith.samala@stonybrook.edu]>
2023-08-05 17:03:40 -0400

    added message root and staticfiles dir

--- capstone_project/settings.py
+++ capstone_project/settings.py
@@ -122,6 +122,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = 'debugger/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
