7272c4987e7fdd852b091dd30253052d802847f9
[Aadith Samala] <[aadith.samala@stonybrook.edu]>
2023-08-06 09:50:44 -0400

    removed import urls

--- capstone_project/urls.py
+++ capstone_project/urls.py
@@ -20,12 +20,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("django.contrib.auth.urls")),
     path('', include("debugger.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
